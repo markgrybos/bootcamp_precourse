b7ca39266045e274f80ae64f64b097b8c7dbbd04
markgrybos <markgrybos@gmail.com>
2021-05-19 01:07:52 -0700

    second commit

---
+++ w3schools_pre_course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace w3schools_pre_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
