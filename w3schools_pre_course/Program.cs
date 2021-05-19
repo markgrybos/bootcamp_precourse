a57a3f5cf2ed9d2afae8df0c37d7a994149bba29
markgrybos <markgrybos@gmail.com>
2021-05-19 01:17:32 -0700

    third commit

--- w3schools_pre_course/Program.cs
+++ w3schools_pre_course/Program.cs
@@ -6,7 +6,8 @@ namespace w3schools_pre_course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mark");
+            Console.ReadKey();
         }
     }
 }
